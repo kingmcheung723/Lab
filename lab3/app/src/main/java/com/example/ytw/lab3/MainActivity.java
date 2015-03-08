763bcf98964c2b387ea331c3f64f867caa5ceb29
King Cheung <kingmcheung723@gmail.com>
2015-03-08 15:56:53 +0800

    lab 3 part 1 finished

--- lab3/app/src/main/java/com/example/ytw/lab3/MainActivity.java
+++ lab3/app/src/main/java/com/example/ytw/lab3/MainActivity.java
@@ -1,39 +1,83 @@
 package com.example.ytw.lab3;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private ProgressBar progressBar;
+    private TextView statusText;
+    private int completed;
+    private Runnable worker = new Runnable() {
+        public void run() {
+            int l;
+            // Initialize the progress bar and the status TextView
+            completed = 0;
+            // we want to modify the progress bar so we need to do it from the UI thread
+            // how can we make sure the code runs in the UI thread? use the handler!
 
-public class MainActivity extends ActionBarActivity {
+            // Do a large amount of computation
+            for (int i = 0; i < 100; ++i) {
+                for (int j = 0; j < 5000; ++j) {
+                    for (int k = 0; k < 5000; ++k) {
+                        l = i * j * k;
+                    }
+                }
+                completed += 1;
+                handler.post(new Runnable() {
+                    public void run() {
+                        // Here the worker thread uses the post method to initialize the progress bar.
+                        // Finish this method by yourself.
+                        // Periodically update the UI to reflect the progress of the computation
+                        progressBar.setProgress(completed);
+                        statusText.setText(String.format("Completed %d", completed));
+                    }
+                });
+                // Here the worker thread do the loop and update the progress bar periodically
+                // Similarly, it uses the handler.post method to update the progress bar
+                // Finish this part by yourself.
+            }
+        }
+    };
+    private Handler handler;
 
+   /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get handles for the progress bar and status text TextView
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        statusText = (TextView) findViewById(R.id.status_text);
+        // Set the maximum value that the progress bar will display
+        progressBar.setMax(100);
+        // Declare the listeners for the two buttons
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(this);
+
+        handler = new Handler();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View source) {
+        // Start button is clicked
+        if (source.getId() == R.id.start_button) {
+            // Declare a new background thread
+            // Start the background thread
+            Thread workerThread = new Thread(worker);
+            workerThread.start();
+        } else if (source.getId() == R.id.reset_button) {
+            // Reset button is clicked
+            completed = 0;
+            progressBar.setProgress(completed);
+            statusText.setText(String.format("Click the button"));
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
