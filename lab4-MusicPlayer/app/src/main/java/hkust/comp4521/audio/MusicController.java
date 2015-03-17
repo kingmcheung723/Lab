fe00f5b33c04176cfa12c99e80841da26ec2d7f4
King Cheung <kingmcheung723@gmail.com>
2015-03-18 01:48:25 +0800

    Point 53 finished

--- lab4-MusicPlayer/app/src/main/java/hkust/comp4521/audio/MusicController.java
+++ lab4-MusicPlayer/app/src/main/java/hkust/comp4521/audio/MusicController.java
@@ -1,8 +1,15 @@
-package	hkust.comp4521.audio;
-import	android.app.Service;
-import	android.content.Intent;
+package hkust.comp4521.audio;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
-import	android.os.IBinder;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,31 +21,62 @@ import hkust.comp4521.audio.player.MusicPlayer;
 //	startService(),	only	the	onStartCommand()	method	is	executed. This	method	will	handle	the	
 //	incoming	intents	when	startService()	is	called	from	the	activity/fragment
 
-public class MusicController	extends Service	implements MediaPlayer.OnErrorListener {
-    private static final String	TAG =	"MusicController:";
-    MusicPlayer	player =	null;
-    private static int songIndex =	0;
+public class MusicController extends Service implements MediaPlayer.OnErrorListener {
+    private static final String TAG = "MusicController:";
+    MusicPlayer player = null;
+    private static int songIndex = 0;
+    private NotificationManager mNotificationManager;
+    // noteId allows  you to  update the notification later on.
+    private int noteId = 1;
+
+    // put the notification into the notification bar. This  method is called  when the  song  is first
+    //  initialized.  It will  be  updated  with  control buttons  by updateNotification().
+    private void putNotification() {
+        Bitmap largeIcon;
+        largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.note_blue);
+        Notification.Builder mBuilder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.listen_icon)
+                .setLargeIcon(largeIcon.createScaledBitmap(largeIcon, 72, 72, false))
+                .setOngoing(true)
+                .setContentTitle("Music Player").setContentText("");
+        //  Creates  an  explicit   intent  for the  MusicActivity Activity
+        Intent resultIntent = new Intent(this, MusicActivity.class);
+        // create   a  pending    intent    that   will   be   fired  when   notification  is touched.
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+        mBuilder.setContentIntent(resultPendingIntent);
+        Log.i(TAG, "Service: putNotification()");
+        // noteId  allows  you to  update the notification  later on.
+        // set the  service  as  a  foreground  service
+        startForeground(noteId, mBuilder.build());
+    }
+
+    private void cancelNotification() {
+        mNotificationManager.cancel(noteId);
+        stopForeground(true);
+    }
+
     @Override
-    public	boolean	onError(MediaPlayer	mp,	int	what,	int	extra)	{
-        Toast.makeText(this,	"MusicController	Music	Player	Failed",	Toast.LENGTH_SHORT).show();
-        Log.i(TAG,	"Service:	Music	player	failed");
-        if	(mp	!=	null)	{
-            try	{
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Toast.makeText(this, "MusicController	Music	Player	Failed", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "Service:	Music	player	failed");
+        if (mp != null) {
+            try {
                 mp.stop();
                 mp.release();
-            }
-            finally	{
-                mp	=	null;
+            } finally {
+                mp = null;
             }
         }
-        return	false;
+        return false;
     }
+
     @Override
-    public	IBinder	onBind(Intent	intent)	{
-        return	null;
+    public IBinder onBind(Intent intent) {
+        return null;
     }
+
     @Override
-    public	void	onCreate()	{
+    public void onCreate() {
         super.onCreate();
         //	Pop	up	a	message	on	the	screen	to	show	that	the	service	is	started
         Toast.makeText(this, "MusicController	Service	Created", Toast.LENGTH_SHORT).show();
@@ -48,144 +86,194 @@ public class MusicController	extends Service	implements MediaPlayer.OnErrorListe
         //	method	returns	the	reference	to	the	instance	of	the	music	player	class
         //	get	a	reference	to	sthe	instance	of	the	music	player
         //	set	the	context	for	the	music	player	to	be	this	service
-        player	=	MusicPlayer.getMusicPlayer();
+        player = MusicPlayer.getMusicPlayer();
         player.setContext(this);
+        //get  a  reference   to   the  notification  manager.
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
         startSong(songIndex);
     }
 
-    public	void	startSong(int	index)	{
-        Log.i(TAG,	"Service:	startSong()");
-        final	String[]	songFile	=	getResources().getStringArray(R.array.filename);
-        final	String[]	songList	=	getResources().getStringArray(R.array.Songs);
-        if	(player	!=	null)	{
-            songIndex	=	index;
-            player.start(getResources().getIdentifier(songFile[index],	"raw",
-                    getPackageName()),	songList[index]);
-        }
-        else
-            Log.i(TAG,	"Service:	startSong	Null	Player");
+    //  updateNotification() updates  the  information  in  the  notification and  adds the  player
+//  control buttons.
+    private void updateNotification(Notification.Action action) {
+//  Set   the  notification  style  to  MediaStyle.  This  allows  us  t put  player control
+//  buttons  into   the  notification
+        Notification.MediaStyle style = new Notification.MediaStyle();
+//  We     add in    three action  buttons to the  compact  notification  view (rewind,  play/pause  forward)
+        style.setShowActionsInCompactView(1, 2, 3);
+        Bitmap largeIcon;
+        largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.note_blue);
+        Notification.Builder mBuilder = new
+                Notification.Builder(this)
+                .setSmallIcon(R.drawable.listen_icon)
+                .setLargeIcon(largeIcon.createScaledBitmap(largeIcon, 72, 72, false))
+                .setOngoing(true)
+                .setContentTitle("Music  Player").setContentText(player.getSongTitle()).setStyle(style);
+//  five    action  buttons  are  added  to  the notification.
+        mBuilder.addAction(createAction(android.R.drawable.ic_media_previous, "Previous", Constants.ACTION_PREVIOUS));
+        mBuilder.addAction(createAction(android.R.drawable.ic_media_rew, "Rewind", Constants.ACTION_REWIND));
+        mBuilder.addAction(action);
+        mBuilder.addAction(createAction(android.R.drawable.ic_media_ff, "Fast Foward", Constants.ACTION_FORWARD));
+
+        mBuilder.addAction(createAction(android.R.drawable.ic_media_next, "Next", Constants.ACTION_NEXT));
+//  Creates     an  explicit   intent for  the  MusicActivity  Activity
+        Intent resultIntent = new Intent(this, MusicActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+        mBuilder.setContentIntent(resultPendingIntent);
+        Log.i(TAG, "Service: putNotification()");
+//  noteId allows  you to   update the  notification   later  on.
+        mNotificationManager.notify(noteId,
+                mBuilder.build());
+    }
+
+    public void play_pause() {
+        if (player != null) {
+// if  player        is  playing,   then  abandon  audio  focus.  we  are  pausing  playback
+//  else,  get    audio  focus  before  we proceed  to play the song.
+            if (player.isPlaying()) {
+// update       the  pause    button in  the notification   to  play  button
+                updateNotification(createAction(android.R.drawable.ic_media_play, "Play", Constants.ACTION_PLAY));
+            } else {
+// update       the  pause   button in  the notification   to  pause  button
+                updateNotification(createAction(android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE));
+            }
+            Log.i(TAG, "Service: play_pause()");
+            player.play_pause();
+        } else
+            Log.i(TAG, "Service: play_pause() Null  Player");
+    }
+
+    //  This   method  creates  the action associated  with   the  button in  the  Notification.
+//  This   creates  a pending  Intent   and  associates it  with the  button  click.
+    private Notification.Action createAction(int icon, String title, String intentAction) {
+        Intent intent = new Intent(getApplicationContext(), MusicController.class);
+        intent.setAction(intentAction);
+        PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
+        return new Notification.Action.Builder(icon, title, pendingIntent).build();
+    }
+
+    public void startSong(int index) {
+        Log.i(TAG, "Service:	startSong()");
+        final String[] songFile = getResources().getStringArray(R.array.filename);
+        final String[] songList = getResources().getStringArray(R.array.Songs);
+        if (player != null) {
+            songIndex = index;
+            player.start(getResources().getIdentifier(songFile[index], "raw",
+                    getPackageName()), songList[index]);
+            putNotification();
+        } else
+            Log.i(TAG, "Service:	startSong	Null	Player");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
     }
-    public	void	play_pause()	{
-        if	(player	!=	null)	{
-            //	if	player	is	playing,	then	abandon	audio	focus.	we	are	pausing	playback
-            //	else,	get	audio	focus	before	we	proceed	to	play	the	song.
-            if	(player.isPlaying())	{
-                //	update	the	pause	button	in	the	notification	to	play	button
-            }
-            else	{
-                //	update	the	pause	button	in	the	notification	to	pause	button
-            }
-            Log.i(TAG,	"Service:	play_pause()");
-            player.play_pause();
-            //	update	notification	with	song	information
-        }
-        else
-            Log.i(TAG,	"Service:	play_pause()	Null	Player");
-    }
-    public	void	resume()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	resume()");
+
+    public void resume() {
+        if (player != null) {
+            Log.i(TAG, "Service:	resume()");
             player.resume();
             //	update	the	pause	button	in	the	notification	to	pause	button
-        }
-        else
-            Log.i(TAG,	"Service:	resume()	Null	Player");
+            updateNotification(createAction(android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE));
+        } else
+            Log.i(TAG, "Service:	resume()	Null	Player");
     }
-    public	void	pause()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	pause()");
+
+    public void pause() {
+        if (player != null) {
+            Log.i(TAG, "Service:	pause()");
             player.pause();
             //	update	the	pause	button	in	the	notification	to	play	button
-        }
-        else
-            Log.i(TAG,	"Service:	pause()	Null	Player");
+            updateNotification(createAction(android.R.drawable.ic_media_play, "Pause", Constants.ACTION_PLAY));
+        } else
+            Log.i(TAG, "Service:	pause()	Null	Player");
     }
-    public	void	rewind()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	rewind()");
+
+    public void rewind() {
+        if (player != null) {
+            Log.i(TAG, "Service:	rewind()");
             player.rewind();
 
-        }
-        else
-            Log.i(TAG,	"Service:	rewind()	Null	Player");
+        } else
+            Log.i(TAG, "Service:	rewind()	Null	Player");
     }
-    public	void	forward()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	forward()");
+
+    public void forward() {
+        if (player != null) {
+            Log.i(TAG, "Service:	forward()");
             player.forward();
-        }
-        else
-            Log.i(TAG,	"Service:	forward()	Null	Player");
+        } else
+            Log.i(TAG, "Service:	forward()	Null	Player");
     }
-    public	void	stop()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	stop()");
+
+    public void stop() {
+        if (player != null) {
+            Log.i(TAG, "Service:	stop()");
             player.stop();
-        }
-        else
-            Log.i(TAG,	"Service:	stop()	Null	Player");
+        } else
+            Log.i(TAG, "Service:	stop()	Null	Player");
     }
-    public	void	reset()	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	reset()");
+
+    public void reset() {
+        if (player != null) {
+            Log.i(TAG, "Service:	reset()");
             player.reset();
-        }
-        else
-            Log.i(TAG,	"Service:	reset()	Null	Player");
+            cancelNotification();
+        } else
+            Log.i(TAG, "Service:	reset()	Null	Player");
     }
-    public	void	reposition(int	position)	{
-        if	(player	!=	null)	{
-            Log.i(TAG,	"Service:	reposition()");
+
+    public void reposition(int position) {
+        if (player != null) {
+            Log.i(TAG, "Service:	reposition()");
             player.reposition(position);
-        }
-        else
-            Log.i(TAG,	"Service:	reposition()	Null	Player");
+        } else
+            Log.i(TAG, "Service:	reposition()	Null	Player");
     }
+
     //	handle	the	intent	delivered	to	onStartCommand().
-    private	void	handleIntent(	Intent	intent	)	{
-        if(	intent	==	null	||	intent.getAction()	==	null	)
+    private void handleIntent(Intent intent) {
+        if (intent == null || intent.getAction() == null)
             return;
         //	get	the	action	specified	in	the	intent.	The	actioins	are	given	in	Constants.
-        String	action	=	intent.getAction();
-        if(	action.equalsIgnoreCase(Constants.ACTION_PLAY_PAUSE	)	)	{
+        String action = intent.getAction();
+        if (action.equalsIgnoreCase(Constants.ACTION_PLAY_PAUSE)) {
             play_pause();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_PLAY	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_PLAY)) {
             resume();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_PAUSE )	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_PAUSE)) {
             play_pause();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_FORWARD	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_FORWARD)) {
             forward();
 
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_REWIND	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_REWIND)) {
             rewind();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_PREVIOUS	)	)	{
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_NEXT	)	)	{
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_STOP	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_PREVIOUS)) {
+        } else if (action.equalsIgnoreCase(Constants.ACTION_NEXT)) {
+        } else if (action.equalsIgnoreCase(Constants.ACTION_STOP)) {
             stop();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_RESET	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_RESET)) {
             reset();
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_SONG	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_SONG)) {
             reset();
-            int	id	=	intent.getIntExtra("Song",0);
+            int id = intent.getIntExtra("Song", 0);
             startSong(id);
-        }	else	if(	action.equalsIgnoreCase(Constants.ACTION_REPOSITION	)	)	{
-            int	position	=	intent.getIntExtra("Position",0);
+        } else if (action.equalsIgnoreCase(Constants.ACTION_REPOSITION)) {
+            int position = intent.getIntExtra("Position", 0);
             reposition(position);
-        } else	if(	action.equalsIgnoreCase(Constants.ACTION_COMPLETED	)	)	{
+        } else if (action.equalsIgnoreCase(Constants.ACTION_COMPLETED)) {
             reset();
             startSong(songIndex);
         }
     }
+
     @Override
-    public	int	onStartCommand(Intent	intent,	int	flags,	int	startId)	{
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "Service: onStartCommand()");
         //	Any	time	startService()	is	called,	the	intent	is	delivered	here	and	can	be	handled.
         handleIntent(intent);
-        return super.onStartCommand(intent,	flags,startId);
+        return super.onStartCommand(intent, flags, startId);
     }
 }
